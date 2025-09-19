6855a8acff2e6635c3b6802fda59da4045b68e0a
Wassim Maziane <wassim.maziane@theodo.com>
2025-09-19 17:00:57 +0100

    refactor: added route and image objects to centralize their configs

---
+++ src/config/images.ts
@@ -0,0 +1,4 @@
+export const IMAGES = {
+  userIcon: "/assets/user.svg",
+  theodoLogo: "/assets/theodo-logo.svg",
+};
---
+++ src/config/routes.ts
@@ -0,0 +1,4 @@
+export const ROUTES = {
+  signIn: "/signin",
+  singUp: "/signup",
+};
