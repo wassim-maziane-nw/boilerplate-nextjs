d2fcb445c227915beb60ad29ac80040b240d72b5
Wassim Maziane <wassim.maziane@theodo.com>
2025-09-19 16:14:02 +0100

    test: add tests for routing in home page component

--- src/__test__/home.snap.tsx
+++ src/__test__/home.snap.tsx
@@ -2,6 +2,14 @@ import HomePage from "~app/page";
 
 import { render } from "./test-utils";
 
+const mockRouter = {
+  push: jest.fn(),
+};
+
+jest.mock("next/navigation", () => ({
+  useRouter: () => mockRouter,
+}));
+
 describe("Testing page example", () => {
   it("should render home", () => {
     const { container } = render(<HomePage />);
--- src/__test__/home.spec.tsx
+++ src/__test__/home.spec.tsx
@@ -1,11 +1,26 @@
 import Home from "~components/home/Home";
 
-import { render } from "./test-utils";
+import { render, userEvent } from "./test-utils";
+
+const mockRouter = {
+  push: jest.fn(),
+};
+
+jest.mock("next/navigation", () => ({
+  useRouter: () => mockRouter,
+}));
 
 describe("Testing Home page component", () => {
   it("should find the background image", () => {
     const { container } = render(<Home />);
     const backgroundDiv = container.getElementsByClassName("background-div");
     expect(backgroundDiv.length).toBe(1);
+  });
+  it("test", async () => {
+    const { getByRole } = render(<Home />);
+    const button = getByRole("button", { name: /Se connecter/u });
+    const user = userEvent.setup();
+    await user.click(button);
+    expect(mockRouter.push).toHaveBeenCalledWith("/signin");
   });
 });
