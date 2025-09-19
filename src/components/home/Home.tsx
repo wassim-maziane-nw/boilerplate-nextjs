2995c08a7506408277d8d857e854ca1276ca785d
Wassim Maziane <wassim.maziane@theodo.com>
2025-09-19 17:00:57 +0100

    refactor: added route and image objects to centralize their configs

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,6 +2,8 @@
 
 import PrimaryButton from "~components/primary-button";
 import SecondaryButton from "~components/secondary-button";
+import { IMAGES } from "~config/images";
+import { ROUTES } from "~config/routes";
 import txKeys from "~i18n/translations";
 import { useTranslation } from "~i18n/useTranslation";
 
@@ -18,10 +20,10 @@ const Home: FC = () => {
   const translate = useTranslation();
   const user = useLastConnectedUser();
   const handleJoinClick = () => {
-    router.push("/signup");
+    router.push(ROUTES.singUp);
   };
   const handleSigninClick = () => {
-    router.push("/signin");
+    router.push(ROUTES.signIn);
   };
   return (
     <Background className="background-div relative">
@@ -30,7 +32,7 @@ const Home: FC = () => {
           href={{ pathname: "/signin", query: { prefilled: "true" } }}
           className="absolute top-4 right-40 flex gap-2"
         >
-          <Image src="/assets/user.svg" alt="Theodo user" width={14} height={16} />
+          <Image src={IMAGES.userIcon} alt="Theodo user" width={14} height={16} />
           <div>
             <Typography variant="h3" color="silver" component="span">
               {translate(txKeys.common.homePage.continueLink)}
@@ -41,7 +43,7 @@ const Home: FC = () => {
       )}
       <div className="flex justify-center items-center h-full">
         <div className="flex flex-col justify-center items-center h-full w-1/2 gap-4">
-          <Image src="/assets/theodo-logo.svg" alt="Theodo Logo" width={295} height={63} />
+          <Image src={IMAGES.theodoLogo} alt="Theodo Logo" width={295} height={63} />
           <Typography variant="h1">{translate(txKeys.common.homePage.title)}</Typography>
           <Typography variant="h2">{translate(txKeys.common.homePage.subtitle)}</Typography>
           <div className="flex gap-6 pt-8">
